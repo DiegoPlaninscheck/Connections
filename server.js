410b29d373b53c334b8d90fcb602712b5020ef6a
DiegoPlaninscheck <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-06-22 21:59:52 -0300

    feat: update server

--- server.js
+++ server.js
@@ -1,28 +1,58 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const server = express();
 const port = 8080;
 
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize("test", "root", "root", {
-    host: "localhost",
-    dialect: "mysql"
+  host: "localhost",
+  dialect: "mysql",
 });
 
-sequelize.authenticate().then(() => {
+sequelize.authenticate()
+  .then(() => {
     console.log("Connected");
-}).catch(err => {
+  })
+  .catch((err) => {
     console.log("Error: ", err);
-})
+  });
+
+const User = sequelize.define(
+  "users",
+  {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    createdAt: false,
+    updatedAt: false,
+  }
+);
+
+server.post("/user", (req, res) => {
+    const { firstName, lastName } = req.body;
+    User.create({firstName, lastName}).then(() => {
+        res.json({message: "User created successfully"})
+    }).catch(err => {
+        res.json({err: "Error creating user"})
+    });
+});
 
 server.use(cors());
 server.use(express.json());
 
-server.get('/',  (req, res) => {
-    res.json({message: "Server started!"})
-})  
+server.get("/", (req, res) => {
+  res.json({ message: "Server started!" });
+});
 
-server.listen(port, () => {
+server.listen(port, async () => {
+    await sequelize.sync();
     console.log(`Server start at http://localhost:${port}`);
-})
+});
