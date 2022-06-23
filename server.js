32a60b00ed56683f2ebab5c8b32ec588e937c115
DiegoPlaninscheck <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-06-23 09:49:56 -0300

    fix: update server

--- server.js
+++ server.js
@@ -9,48 +9,54 @@ server.use(express.json());
 const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize("test", "root", "root", {
-  host: "localhost",
-  dialect: "mysql",
+    host: "localhost",
+    dialect: "mysql",
 });
 
 sequelize.authenticate()
-  .then(() => {
-    console.log("Connected");
-  })
-  .catch((err) => {
-    console.log("Error: ", err);
-  });
+    .then(() => {
+        console.log("Connected");
+    })
+    .catch((err) => {
+        console.log("Error: ", err);
+    });
 
 const User = sequelize.define(
-  "users",
-  {
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
+    "users",
+    {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
     },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    createdAt: false,
-    updatedAt: false,
-  }
+    {
+        createdAt: false,
+        updatedAt: false,
+    }
 );
 
+server.get("/users", async(req, res) => {
+    const user = await User.findAll();
+    console.log(user);
+    res.status(200).json(user)
+})
+
 server.post("/user", (req, res) => {
-    const {firstName, lastName} = req.body;
-    User.create({firstName, lastName}).then(() => {
-        res.json({message: "User created successfully"})
+    const { firstName, lastName } = req.body;
+    User.create({ firstName, lastName }).then(() => {
+        res.json({ message: "User created successfully" })
     }).catch(err => {
-        res.json({err: "Error creating user"})
+        res.json({ err: "Error creating user" })
     });
 });
 
 
 server.get("/", (req, res) => {
-  res.json({ message: "Server started!" });
+    res.json({ message: "Server started!" });
 });
 
 server.listen(port, async () => {
