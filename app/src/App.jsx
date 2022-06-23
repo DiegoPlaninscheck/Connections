b3686343f94d41dd5388c0746399a21f18393a6b
DiegoPlaninscheck <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-06-23 11:46:04 -0300

    feat: update app

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,44 +1,31 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { useState, useEffect } from 'react'
+import axios from "axios"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    handleData();
+  }, [])
+
+  async function handleData() {
+    const { data } = await axios.get("http://localhost:8080/users")
+    setData(data);
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <ul>
+          {data.map(user => {
+            return (
+              <li key={user.id}>
+                {user.firstName}
+              </li>
+            )
+          })}
+      </ul>
+    </>
   )
 }
 
